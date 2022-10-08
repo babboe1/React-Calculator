09394dd2dcbd368cd82e8c89ce0650b452d39fbe
babboeCodes <babboecodes@gmail.com>
2022-10-08 09:55:30 +0100

    create component

---
+++ src/components/CalcButtons/CalcInput/CalcInput.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import Context from '../../ContextApi/Context';
+
+const CalcInput = ( props ) => {
+   const context = useContext( Context );
+   context.inputClickHandler = ( e ) => {
+      
+      context.operation += e.target.value;
+      context.setScreen();
+      if ( [ '+', '-', '/', '*' ].includes( e.target.value ) ) {
+      } 
+   };
+   return (
+      <input
+         type="button"
+         className={`button ${props.class}`}
+         value={props.text}
+         onClick={context.inputClickHandler}
+      />
+   );
+};
+
+export default CalcInput;
